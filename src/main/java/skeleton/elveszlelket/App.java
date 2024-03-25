201f3531dc1fc17780c2bea22023270e4530cda0
Lukácsi Sándor <sandor.lukacsi@evocrm.hu>
2024-03-25 11:46:28 +0100

    javadoc

--- src/main/java/skeleton/elveszlelket/App.java
+++ src/main/java/skeleton/elveszlelket/App.java
@@ -1,13 +1,16 @@
 package skeleton.elveszlelket;
 import skeleton.elveszlelket.tester.Tester;
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static Tester t;
 
+    /**
+     * Az alkalmazás belépési pontja.
+     * Létrehoz egy Tester példányt és elindítja a parancsok feldolgozását.
+     *
+     * @param args
+     */
     public static void main( String[] args )
     {
         t = new Tester();
