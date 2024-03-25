7272fc04658b32aeac37b17fc82d505e4f44445a
Lukácsi Sándor <sandor.lukacsi@evocrm.hu>
2024-03-25 12:11:57 +0100

    javadoc and removeitem

--- src/main/java/skeleton/elveszlelket/tester/Commands/CHECKWIN.java
+++ src/main/java/skeleton/elveszlelket/tester/Commands/CHECKWIN.java
@@ -2,7 +2,18 @@ package skeleton.elveszlelket.tester.Commands;
 import skeleton.elveszlelket.Student;
 import skeleton.elveszlelket.tester.Tester;
 
+/**
+ * Egy parancsot implementál, amely ellenőrzi, hogy a játékban részt vevő diákok megnyerték-e a játékot.
+ * A nyerés feltétele, hogy minden diáknak rendelkeznie kell a Logar tárggyal.
+ */
 public class CHECKWIN implements skeleton.elveszlelket.tester.Commands.Command {
+    /**
+     * Ellenőrzi, hogy minden diáknak van-e Logar tárgya a szimulációban.
+     * Ha minden diáknak van, akkor a játék nyertes állapotba kerül.
+     *
+     * @param params A parancs paraméterei (nem használja).
+     * @param t A Tester objektum, amely a diákok gyűjteményét tartalmazza.
+     */
     public void execute(String[] params, Tester t) {
         boolean win = true;
         for (Student hallgato : t.getStudents()) {
