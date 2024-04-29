dd6c05fc570dba4d751d194140cf0254c9e28714
Maródi Gergő <marigeri37@gmail.com>
2024-04-29 11:00:06 +0200

    CLEAR-fix

--- src/main/java/skeleton/elveszlelket/tester/Commands/CLEAR.java
+++ src/main/java/skeleton/elveszlelket/tester/Commands/CLEAR.java
@@ -7,6 +7,7 @@ public class CLEAR implements Command {
     @Override
     public void execute(String[] params, Tester t) {
         t.clear();
+        System.out.println("Az osszes objektum torolve. A rendszer alapallapotban van.");
     }
 
 }
