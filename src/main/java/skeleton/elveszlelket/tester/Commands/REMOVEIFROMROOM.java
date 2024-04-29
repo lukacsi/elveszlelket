bda80c835cdb4e462c1b3464eb83bf0e8a317d04
Maródi Gergő <marigeri37@gmail.com>
2024-04-29 12:41:35 +0200

    REMOVEIFROMROOM-fix

--- src/main/java/skeleton/elveszlelket/tester/Commands/REMOVEIFROMROOM.java
+++ src/main/java/skeleton/elveszlelket/tester/Commands/REMOVEIFROMROOM.java
@@ -12,10 +12,10 @@ public class REMOVEIFROMROOM implements Command {
             Room room = t.getRoom(params[2]);
 
             if (item == null) {
-                System.out.println("Nincs ilyen targy.");
+                System.out.println("Parameterul kapott "+params[1]+" nincs feljegyezve.");
             }
             if (room == null) {
-                System.out.println("Nincs ilyen szoba.");
+                System.out.println("Parameterul kapott "+params[2]+" nincs feljegyezve.");
             }
             boolean hasItem = false;
             for (Item it : room.getItems()) {
@@ -24,11 +24,12 @@ public class REMOVEIFROMROOM implements Command {
                 }
             }
             if (!hasItem) {
-                System.out.println("Nincs a szobaban ilyen targy.");
+                System.out.println(params[1]+"nem lett eltavolitva "+params[2]+" targyai kozul. Nincs benne.");
             }
             if (item != null && room != null && hasItem) {
                 room.removeItem(item);
                 item.setRoom(null);
+                System.out.println(params[1]+" eltavolitva "+params[2]+" targyai kozul.");
             }
         } else {
             System.out.println("A parameterek szama nem megfelelo.");
